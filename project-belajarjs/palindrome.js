190fa333f462ca2186ddc4a653e3d052dda1d8d6
Qudrotul Ichsan Saputra <32732522+Qudrotulichsan@users.noreply.github.com>
2022-02-06 16:54:53 +0700

    Add files via upload
    16:54 06/02/2022

---
+++ project-belajarjs/palindrome.js
@@ -0,0 +1,14 @@
+var kata = "kikik";
+var balikkata = '';
+
+for(let i=kata.length -1; i >=0; i--){
+    balikkata += kata[i];
+
+}
+console.log(balikkata);
+
+if(balikkata == "kikik"){
+    console.log('true');
+} else {
+    console.log('false');
+}
